c7c7abe29d9ca858b149c38ae315d5ef367c0da9
gthandavam <gthandavam@gmail.com>
2013-04-12 20:30:31 -0400

    Stemming of the context words. As per the paper, they remove stop words, rare words and then stem the remaining words.
    1. Still need to figure out how to remove rare words.
    2. I didnt evaluate other stemmers available in NLTK (if at all they
    exist) - Got to read about the difference between Lemmatizing and
    stemming. Paper says stemming was done - Question here is whether
    Lemmatizing will add any value to the system ?

--- ParseWikiDump/gt/esa/parse/parse_wiki.py
+++ ParseWikiDump/gt/esa/parse/parse_wiki.py
@@ -5,18 +5,21 @@ Created on Apr 11, 2013
 '''
 
 import xml.etree.ElementTree as etree
+#beautiful soup library to convert html entities to text - overkill
 from bs4 import BeautifulSoup as bs
 import re
 from nltk.corpus import stopwords as sw
- #beautiful soup library to convert html entities to text - overkill
- 
-def rem_stop_words(text):
+from nltk.stem.porter import PorterStemmer
+
+
+def rem_stop_words_and_stem(text):
     #split based on one or more whitespace
+    p_stemmer = PorterStemmer()
     words = text.split()
     text = ''
     for word in words:
         if(word not in sw.words('english')):
-            text = text + word + ' '
+            text = text + p_stemmer.stem(word) + ' '
             
     return text
  
@@ -29,18 +32,25 @@ if __name__ == '__main__':
     tree = etree.parse('small_dump')
     encoding = 'UTF-8'
     pages = tree.findall('.//page')
-    
+#     word_dict = {}
     for page in pages:
         print page.attrib['id']
         title = bs(etree.tostring(page.find('title'))).text
         print title
         text = bs(etree.tostring(page.find('text'))).text
-        text = rem_stop_words(rem_punctuation(text.lower()))
+        text = rem_stop_words_and_stem(rem_punctuation(text.lower()))
         print text
+#         words = text.split()
+#         for word in words:
+#             if( word in word_dict ):
+#                 word_dict[word] += 1
+#             else: 
+#                 word_dict[word] = 1
         print "******************************************"
     
     
-    
-    
-      
-        
+#     
+#     for word in word_dict.keys():
+#         print word, " ", word_dict[word]
+#       
+#         
