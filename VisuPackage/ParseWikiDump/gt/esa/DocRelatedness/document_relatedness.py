83b558e324e129de79be09301c03d686c894e931
Omar Khazamov <khazamov@gmail.com>
2013-05-02 14:18:32 -0400

    Cosine similarity for all words in small dump. Need to put them in DB

--- VisuPackage/ParseWikiDump/gt/esa/DocRelatedness/document_relatedness.py
+++ VisuPackage/ParseWikiDump/gt/esa/DocRelatedness/document_relatedness.py
@@ -3,7 +3,7 @@ Created on Apr 11, 2013
 computes cosine similarity
 @author: Okhazamov
 '''
-
+#!/usr/bin/python
 import xml.etree.ElementTree as etree
 from itertools import cycle
 #beautiful soup library to convert html entities to text - overkill
@@ -22,8 +22,10 @@ import numpy as np
 from scipy.spatial import distance
 from  curses.ascii import * 
 import numpy.linalg as LA
-
-size_of_vocab=700
+import os.path
+#number of words
+#which is half the number of files in inverted_tf file
+size_of_vocab=4000
 def cosine_similarity(u,v):
     dist=distance.cosine(u, v)
     return dist
@@ -67,23 +69,42 @@ def Read_IDF_Array(filename, A):
     f1.close()
 
 if __name__ == '__main__':
-        cosine_distance = lambda a, b : round(np.inner(a, b)/(LA.norm(a)*LA.norm(b)), 3)
+    try:	
+	#define cosine distance
+	cosine_distance = lambda a, b : round(np.inner(a, b)/(LA.norm(a)*LA.norm(b)))
+	#nlogn file reads will be performed, position - is position pointer
+	position=0
         stemmer=PorterStemmer()
-        word1=stemmer.stem("article")
-        word2=stemmer.stem("text")
-        
-        
-        
-        A = np.ones((size_of_vocab,1), np.float32 )
-        B = np.ones((size_of_vocab,1), np.float32 )
-        Read_IDF_Array("/home/user/workspace/VisuWords/perl_parse_wiki/inverted_tf/"+word1+"_inverted_idx",A)
-        Read_IDF_Array("/home/user/workspace/VisuWords/perl_parse_wiki/inverted_tf/"+word2+"_inverted_idx",B)
-        Aflat = np.hstack( A )
-        Bflat = np.hstack( B )
-        dist = distance.cosine(Aflat,Bflat)
+	word_file=open("idf_file","r")
+	word1=word_file.readline()
+	while word1:
+  		word1=stemmer.stem(word1).rstrip('1234567890\n\t\r ')
 
-        
-        print "Cosine distance"
-        print dist                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-    
+        	A = np.ones((size_of_vocab,1), np.float32)
+		print "word1",word1	
+		filename1="/home/user/workspace/VisuWords/perl_parse_wiki/inverted_tf/"+word1+"_inverted_idx"	
+	
+		#exists=os.path.exists(filename1)
+		#print filename1, exists
+		if os.path.exists(filename1):		
+			Read_IDF_Array(filename1,A)
+			Aflat = np.hstack(A)
+			word2=word_file.readline()
+        		while word2:
+				print "word2",word2
+                        	word2=stemmer.stem(word2).rstrip('1234567890\n\t\r ')
+                        	filename2="/home/user/workspace/VisuWords/perl_parse_wiki/inverted_tf/"+word2+"_inverted_idx"
+				B = np.ones((size_of_vocab,1), np.float32)
+				if os.path.exists(filename2):                        
+					Read_IDF_Array(filename2,B)
+				Bflat = np.hstack(B)
+        	        	dist = distance.cosine(Aflat,Bflat)
+				print "Cosine distance between ",word1,word2,"  ",dist,"\n"
+				word2=word_file.readline()
+		position+=len(word1)+1        	
+		word_file.seek(position,0)
+		word1=word_file.readline()
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+    except  IOError:
+	print "File error, possibly unknown word"
      
