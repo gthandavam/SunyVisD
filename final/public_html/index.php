b8c9dfe5e3fb1e20da02c298fbdc8b3e0e52ad1c
roukaour <remy.oukaour@gmail.com>
2013-05-07 23:42:24 -0400

    Updated CSS.

--- final/public_html/index.php
+++ final/public_html/index.php
@@ -1,3 +1,9 @@
+<?php
+
+require_once 'porter.php';
+
+?>
+
 <!DOCTYPE html>
 
 <html>
@@ -5,6 +11,7 @@
 <head>
 <meta charset="UTF-8">
 <title>CSE 507: Explicit Semantic Analysis</title>
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700">
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
@@ -18,7 +25,7 @@
 
 <div id="content">
 <div class="central">
-<form id="tfidf" action="index.php" method="post">
+<form id="tfidf" action="<?=htmlentities($_SERVER['PHP_SELF']) ?>" method="post">
 <p>Enter a single word or phrase to find its closest 10 Wikipedia concepts.</p>
 <p><input id="text1" name="text1" type="text" size="40"></p>
 <p>Enter two to calculate their semantic relatedness based on their concept vectors.</p>
