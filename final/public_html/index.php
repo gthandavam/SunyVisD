d4815fae9b1598d203d1bee9a37d19dd12115cb6
roukaour <remy.oukaour@gmail.com>
2013-05-08 19:46:04 -0400

    Semantic relatedness measure works.

--- final/public_html/index.php
+++ final/public_html/index.php
@@ -8,7 +8,8 @@ require_once 'porter.php';
 require_once 'tfidf.php';
 
 $error = NULL;
-$top10 = NULL;
+$top = NULL;
+$top_n = 10;
 $similarity = NULL;
 
 if (array_key_exists('action', $_POST) && $_POST['action'] == 'Submit') {
@@ -18,7 +19,9 @@ if (array_key_exists('action', $_POST) && $_POST['action'] == 'Submit') {
 		$error = 'Please enter a word or phrase!';
 	}
 	elseif (empty($text2)) {
-		$top10 = top10concepts($text1);
+		$top_n = intval($_POST['topn']);
+		if ($top_n <= 0) { $top_n = 10; }
+		$top = top_concepts($text1, $top_n);
 	}
 	else {
 		$similarity = relatedness($text1, $text2);
@@ -50,10 +53,10 @@ if (array_key_exists('action', $_POST) && $_POST['action'] == 'Submit') {
 <div class="central">
 <?php if (!is_null($error)): ?>
 <p class="error"><?=htmlentities($error) ?></p>
-<?php elseif (!is_null($top10)): ?>
-<p class="success">The top 10 closest related concepts to <strong>&ldquo;<?=htmlentities($text1) ?>&rdquo;</strong> are:</p>
+<?php elseif (!is_null($top)): ?>
+<p class="success">The top <strong><?=$top_n ?></strong> closest related concepts to <strong>&ldquo;<?=htmlentities($text1) ?>&rdquo;</strong> are:</p>
 <ol>
-<?php foreach ($top10 as $pair): ?>
+<?php foreach ($top as $pair): ?>
 <?php list($concept, $tfidf) = $pair; ?>
 <li class="success"><strong><a href="https://en.wikipedia.org/wiki/<?=htmlentities($concept) ?>"><?=htmlentities($concept) ?></a></strong> <em>(TF-IDF score = <?=$tfidf ?>)</em></li>
 <?php endforeach; ?>
@@ -61,8 +64,17 @@ if (array_key_exists('action', $_POST) && $_POST['action'] == 'Submit') {
 <?php elseif (!is_null($similarity)): ?>
 <p class="success">The semantic relatedness of <strong>&ldquo;<?=htmlentities($text1) ?>&rdquo;</strong> and <strong>&ldquo;<?=htmlentities($text2) ?>&rdquo;</strong> is <strong><?=$similarity ?></strong>.</p>
 <?php endif; ?>
+<?php if (!is_null($top) || !is_null($similarity)): ?>
+<p class="success"><em>Total query time: <?=$query_time ?>s</em></p>
+<?php endif; ?>
 <form id="tfidf" action="<?=htmlentities($_SERVER['PHP_SELF']) ?>" method="post">
-<p>Enter a single word or phrase to find its closest 10 Wikipedia concepts.</p>
+<p>Enter a single word or phrase to find its closest <select id="topn" name="topn">
+	<option value="1">1</option>
+	<option value="5">5</option>
+	<option value="10" selected="selected">10</option>
+	<option value="20">20</option>
+	<option value="50">50</option>
+</select> Wikipedia concepts.</p>
 <p><input id="text1" name="text1" type="text" size="40"></p>
 <p>Enter two to calculate their semantic relatedness based on their concept vectors.</p>
 <p><input id="text2" name="text2" type="text" size="40"></p>
--- final/public_html/tfidf.php
+++ final/public_html/tfidf.php
@@ -2,37 +2,86 @@
 
 $stemmer = new PorterStemmer();
 
-$db_path = '/home/remy/Desktop/tfidf-d80-t2.5-indexed.db';
+$db_path = '/home/remy/Desktop/tfidf-d80-t1.75-indexed.db';
 $db = new SQLite3($db_path, SQLITE3_OPEN_READONLY);
+
+$query_time = 0;
 
 function stem3($word) {
 	global $stemmer;
 	return $stemmer->stem($stemmer->stem($stemmer->stem($word)));
 }
 
-function top10concepts($text) {
-	global $db;
+function top_concepts($text, $n = 10) {
+	global $db, $query_time;
 	$words = array_filter(array_map('stem3', preg_split('/\s+/', $text)));
 	$concept_ids = array();
 	$concepts = array();
 	$sql = 'SELECT concept, SUM(tfidf) AS tfidfs FROM inverted_index AS ii JOIN concepts AS cs ON ii.concept_id = cs.id WHERE word_id IN (SELECT id FROM words WHERE word IN(';
 	$sql .= implode(',', array_map(function ($w) { return "'$w'"; }, $words));
-	$sql .= ')) GROUP BY concept_id ORDER BY tfidfs DESC LIMIT 10';
+	$sql .= ')) GROUP BY concept_id ORDER BY tfidfs DESC LIMIT ' . $n;
 	$time_start = microtime(true);
 	$result = $db->query($sql);
 	$time_end = microtime(true);
 	$query_time = $time_end - $time_start;
-	$top10 = array();
+	$top = array();
 	while ($row = $result->fetchArray()) {
 		$concept = $row['concept'];
 		$tfidfs = $row['tfidfs'];
-		$top10[] = array($concept, $tfidfs);
+		$top[] = array($concept, $tfidfs);
 	}
-	return $top10;
+	return $top;
 }
 
 function relatedness($text1, $text2) {
-	return 3.141592653589;
+	global $db, $query_time;
+	$words1 = array_filter(array_map('stem3', preg_split('/\s+/', $text1)));
+	$sql1 = 'SELECT concept_id, SUM(tfidf) AS tfidfs FROM inverted_index AS ii WHERE word_id IN (SELECT id FROM words WHERE word IN(';
+	$sql1 .= implode(',', array_map(function ($w) { return "'$w'"; }, $words1));
+	$sql1 .= ')) GROUP BY concept_id';
+	$time_start = microtime(true);
+	$result = $db->query($sql1);
+	$time_end = microtime(true);
+	$query_time = $time_end - $time_start;
+	$vector1 = array();
+	while ($row = $result->fetchArray()) {
+		$concept_id = $row['concept_id'];
+		$tfidfs = $row['tfidfs'];
+		$vector1[$concept_id] = $tfidfs;
+	}
+	$words2 = array_filter(array_map('stem3', preg_split('/\s+/', $text2)));
+	$sql2 = 'SELECT concept_id, SUM(tfidf) AS tfidfs FROM inverted_index AS ii WHERE word_id IN (SELECT id FROM words WHERE word IN(';
+	$sql2 .= implode(',', array_map(function ($w) { return "'$w'"; }, $words2));
+	$sql2 .= ')) GROUP BY concept_id';
+	$time_start = microtime(true);
+	$result = $db->query($sql2);
+	$time_end = microtime(true);
+	$query_time += $time_end - $time_start;
+	$vector2 = array();
+	while ($row = $result->fetchArray()) {
+		$concept_id = $row['concept_id'];
+		$tfidfs = $row['tfidfs'];
+		$vector2[$concept_id] = $tfidfs;
+	}
+	$dot = 0;
+	$mag1 = 0;
+	$mag2 = 0;
+	foreach ($vector1 as $concept_id => $tfidfs1) {
+		$mag1 += $tfidfs1 * $tfidfs1;
+		if (array_key_exists($concept_id, $vector2)) {
+			$tfidfs2 = $vector2[$concept_id];
+			$dot += $tfidfs1 * $tfidfs2;
+			$mag2 += $tfidfs2 * $tfidfs2;
+		}
+	}
+	foreach ($vector2 as $concept_id => $tfidfs2) {
+		if (!array_key_exists($concept_id, $vector1)) {
+			$mag2 += $tfidfs2 * $tfidfs2;
+		}
+	}
+	$mag1 = sqrt($mag1);
+	$mag2 = sqrt($mag2);
+	return $dot / ($mag1 * $mag2);
 }
 
 ?>
