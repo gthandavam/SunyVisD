3231f9f8ff7128c7704d67c2ee2e0c47cb46a229
roukaour <remy.oukaour@gmail.com>
2013-05-07 20:22:31 -0400

    Styled UI with CSS.

--- final/public_html/index.php
+++ final/public_html/index.php
@@ -5,12 +5,36 @@
 <head>
 <meta charset="UTF-8">
 <title>CSE 507: Explicit Semantic Analysis</title>
-<link rel="stylesheet" type="text/css" src="style.css">
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
+
+<div id="header">
+<div class="central">
 <h1>Explicit Semantic Analysis</h1>
-<p>...</p>
+</div>
+</div>
+
+<div id="content">
+<div class="central">
+<form id="tfidf" action="index.php" method="post">
+<p>Enter a single word or phrase to find its closest 10 Wikipedia concepts.</p>
+<p><input id="text1" name="text1" type="text" size="40"></p>
+<p>Enter two to calculate their semantic relatedness based on their concept vectors.</p>
+<p><input id="text2" name="text2" type="text" size="40"></p>
+<p><input id="action" name="action" type="submit" value="Submit"></p>
+</form>
+</div>
+</div>
+
+<div id="footer">
+<div class="central">
+<p>Remy Oukaour, Ganesa Thandavam, Omar Khazamov<br>
+CSE 507, Stony Brook University</p>
+</div>
+</div>
+
 </body>
 
 </html>
